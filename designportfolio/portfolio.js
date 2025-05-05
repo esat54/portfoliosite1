4abc2a9962d7c5c71fc75b02909b993948185ee8
esat <esat@192.168.1.118>
2025-05-06 00:46:34 +0300

    İlk yükleme

---
+++ designportfolio/portfolio.js
@@ -0,0 +1,11 @@
+document.getElementById('home-btn').addEventListener('click', function() {
+    window.location.href = '../designportfolio/portfolio.html';
+});
+
+document.getElementById('contact-btn').addEventListener('click', function() {
+    window.location.href = '../portfolio-iletisim/iletisim.html';
+});
+
+document.getElementById('projects-btn').addEventListener('click', function() {
+    window.location.href = '../portfolio-proje/proje.html';
+});
